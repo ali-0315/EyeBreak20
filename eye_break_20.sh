53e400645e4d8f16d638f32eec9935f48557c5b9
ali0315 <as7490226@gmail.com>
2024-09-29 08:17:30 +0330

    کامیت اولیه

---
+++ eye_break_20.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+while true; do
+    # قفل کردن صفحه‌کلید و موس
+    xtrlock &
+
+    # نمایش تصویر
+    feh -F -Z /opt/eye_break_20/eye_break_warning.png &
+    sleep 20
+
+    # بستن feh و آزاد کردن کیبورد و موس
+    pkill feh
+    pkill xtrlock
+
+    # نمایش پنجره دیالوگ
+    response=$(zenity --title="Eye Break 20" --window-icon=question \
+                      --text="Do you want to continue Eye Break 20 program?" \
+                      --ok-label="Continue" --cancel-label="Quit" \
+                      --timeout=10 --question)
+
+    if [[ "$?" -eq 1 ]]; then
+        break # کاربر گزینه Quit رو انتخاب کرده
+    else
+        # کاربر گزینه Continue رو انتخاب کرده یا زمان پنجره تمام شده
+        sleep 1200
+    fi
+done
